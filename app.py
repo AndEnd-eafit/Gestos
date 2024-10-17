9ace518468a8e63652e483bffbd91c12659048f3
AndEnd-eafit <aprivasm@eafit.edu.co>
2024-10-17 07:50:36 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,23 +5,38 @@ from PIL import Image as Image
 from keras.models import load_model
 import platform
 
-# Muestra la versión de Python junto con detalles adicionales
-st.write(f"<span style='font-family:Lexend;'>Versión de Python: {platform.python_version()}</span>", unsafe_allow_html=True)
+# Inyectar CSS para cambiar las tipografías
+st.markdown("""
+    <style>
+    @import url('https://fonts.googleapis.com/css2?family=Inter:wght@400&family=Lexend:wght@600&display=swap');
+
+    h1, h2, h3 {
+        font-family: 'Lexend', sans-serif;
+    }
+
+    p, div, label, span, input {
+        font-family: 'Inter', sans-serif;
+    }
+    </style>
+    """, unsafe_allow_html=True)
+
+# Muestra la versión de Python con la tipografía 'Inter'
+st.write(f"<span>Versión de Python: {platform.python_version()}</span>", unsafe_allow_html=True)
 
 # Cargar el modelo de Keras
 model = load_model('keras_model.h5')
 data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
 
 # Título con la tipografía Lexend
-st.markdown("<h1 style='font-family:Lexend;'>Reconocimiento de Imágenes</h1>", unsafe_allow_html=True)
+st.markdown("<h1>Reconocimiento de Imágenes</h1>", unsafe_allow_html=True)
 
 # Cargar y mostrar imagen centrada
 image = Image.open('OIG5.jpg')
 st.image(image, width=350, use_column_width='always')
 
 with st.sidebar:
-    st.markdown("<h2 style='font-family:Lexend;'>Usando un modelo entrenado</h2>", unsafe_allow_html=True)
-    st.markdown("<p style='font-family:Inter;'>Puedes usar esta app para identificar gestos.</p>", unsafe_allow_html=True)
+    st.markdown("<h2>Usando un modelo entrenado</h2>", unsafe_allow_html=True)
+    st.markdown("<p>Puedes usar esta app para identificar gestos.</p>", unsafe_allow_html=True)
 
 # Entrada para tomar una foto
 img_file_buffer = st.camera_input("Toma una Foto")
@@ -41,10 +56,6 @@ if img_file_buffer is not None:
 
     # Mostrar resultados
     if prediction[0][0] > 0.5:
-        st.markdown(f"<h2 style='font-family:Lexend;'>Izquierda, con Probabilidad: {str(prediction[0][0])}</h2>", unsafe_allow_html=True)
+        st.markdown(f"<h2>Izquierda, con Probabilidad: {str(prediction[0][0])}</h2>", unsafe_allow_html=True)
     if prediction[0][1] > 0.5:
-        st.markdown(f"<h2 style='font-family:Lexend;'>Arriba, con Probabilidad: {str(prediction[0][1])}</h2>", unsafe_allow_html=True)
-
-    # st.header('Derecha, con Probabilidad: '+str( prediction[0][2]))
-
-
+        st.markdown(f"<h2>Arriba, con Probabilidad: {str(prediction[0][1])}</h2>", unsafe_allow_html=True)
